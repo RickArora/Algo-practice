481b77404526cefe2209e254b56bac8b12a514c8
Ricky Arora <your_email_here>
2020-03-12 14:28:43 -0400

    rodCutting bottom-up done

--- dynamic-programming/rodCutting.py
+++ dynamic-programming/rodCutting.py
@@ -2,24 +2,21 @@
 # Determine the maximum value obtainable by cutting up the rod and selling the pieces 
 import sys
 
-# A Utility function to get the max of 2 ints
-def max(a,b):
-    return a if (a > b) else b 
+INT_MIN = -sys.maxsize-1 #min is initialized to negative numbers
   
-  #Returns the best obtainable price for a rod of length n and price[] as prices of different pieces
-def cutRod(price, n) : 
-  if(n <= 0): # base case
-    return 0
-  max_val = -sys.maxsize-1 # max python integer -1 
-  
-  # Recursivley cut the rod in different pieces and compare different configurations
-  # we set max max size to the most negative integer possible
-  for i in range(0,n): # iterates from 0 to n, using the variable i for the values in between
-    max_val = max(max_val, price[i] + cutRod(price, n - i - 1))  # assigns max val the max val between the previous max val and the new one gained by adding
-    # cutRod(price n - i - 1 )
-  return max_val
-
-
-arr = [1, 5, 8, 9, 10, 17, 17, 20]
-size = len(arr)
-print("Maximum Obtainable Value is " + str(cutRod(arr, size)))
+def cutRod(price, n): # defining cut rod with 2 parameters price and n which is the length of the rod 
+  val = [0 for x in range(n+1)] # initializes an array for 0 to n elements
+  val[0] = 0 # trivial statement since all values in the array val were set to 0 anyways
+  #bottom-up manner
+  for i in range(1, n+1): # iterates from 1 to n+1 in the value i
+    max_val = INT_MIN # min possible value assigned to max_val
+    for j in range(i): #  iterates from 0 to i
+      max_val = max(max_val, price[j] + val[i-j-1]) #assigns max val the maximum value from the current max val and the price[j] +  a precomputed value in val
+    val[i] = max_val # assigns val[i] another entry cached in our memo array
+    print(val[i])
+  return val[n] # returns the last entry, i.e the highest val
+#main
+price = [2,10,15,16,21]
+size = len(price) 
+print("Max value is " + str(cutRod(price, size)))
+    
---
+++ dynamic-programming/rodcutting-naive.py
@@ -0,0 +1,31 @@
+# Given a rod length n inches an an array that contain prices of all pieces of size smaller then n. 
+# Determine the maximum value obtainable by cutting up the rod and selling the pieces 
+# THIS IS A NAIVE SOLUTION>
+import sys
+
+# A Utility function to get the max of 2 ints
+def max(a,b):
+    return a if (a > b) else b 
+  
+  #Returns the best obtainable price for a rod of length n and price[] as prices of different pieces
+def cutRod(price, n) : 
+  if(n <= 0): # base case
+    return 0
+  max_val = -sys.maxsize-1 # max python integer -1 
+  
+  # Recursivley cut the rod in different pieces and compare different configurations
+  # we set max max size to the most negative integer possible
+  for i in range(0,n): # iterates from 0 to n, using the variable i for the values in between
+    max_val = max(max_val, price[i] + cutRod(price, n - i - 1))  # assigns max val the max val between the previous max val and the new one gained by adding
+    # cutRod(price n - i - 1 ), in general the formula for cut rod is
+    # max(price, + cutRod(n-i))
+  return max_val
+
+def cutRod(price, n):
+  val = [0 for x in range(n+1)]
+  val[0] = 0
+  
+
+arr = [1, 5, 8, 9, 10, 17, 17, 20]
+size = len(arr)
+print("Maximum Obtainable Value is " + str(cutRod(arr, size)))
